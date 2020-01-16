01ace55f7a51df4364e5ef97b9f92d4766422a00
adelkaiarullin <adeliarullin@gmail.com>
2020-01-16 21:51:47 +0300

    SINDy algorithm was verified

---
+++ sindy.py
@@ -0,0 +1,23 @@
+import numpy as np
+from sklearn import linear_model
+import lorenz
+
+
+def compute_sindy():
+    clf = linear_model.Lasso(alpha=0.01, max_iter=5000)
+    delta = 1e-2
+    x, y, z = lorenz.get_points(dt=delta, num_steps=10000)
+    c_x, c_y, c_z = x[:-1], y[:-1], z[:-1]
+    p_x, p_y, p_z = x[1:], y[1:], z[1:]
+
+    dmatrix = np.vstack([np.ones_like(c_x), c_x, c_y, c_z, c_x ** 2, c_y ** 2, c_z ** 2,
+        c_x * c_y, c_y * c_z, c_x * c_z, c_x ** 2 * c_y, c_y ** 2 * c_z, c_x ** 2 * c_z])
+
+    #noise = 0.1 * np.random.randn(*dmatrix.T.shape)
+    clf.fit(dmatrix.T , p_y)#(p_x - c_x) / delta)
+    print(clf.coef_)
+
+
+
+if __name__ == '__main__':
+    compute_sindy()
