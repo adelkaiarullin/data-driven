ba81ca4de6b3b75970d04c8a7c434b3d0e6e57ad
adel <adeliarullin@gmail.com>
2020-01-26 14:33:55 +0300

    last hope

--- sindy.py
+++ sindy.py
@@ -6,16 +6,22 @@ import idds
 import lorenz
 
 
-def get_predicted_track(clf, x, step):
+def get_predicted_track(clfs, x, step, dt):
+    x = x.reshape(1, 6)
     track = [x]
 
-    while step > 0:
-        step -= 1
+    k = 0
+    while k < step:
+        print(k, x.shape)
         state = np.zeros(shape=(1, 6), dtype=np.float32)
-        for i, name in enumerate(['x', 'y', 'z', 'vx', 'vy', 'vz']):
-            state[0, i] = clf.predict(get_dynamics(name, *x))
+        for i, (name, clf) in enumerate(zip(['x', 'y', 'z', 'vx', 'vy', 'vz'], clfs)):
+            s = clf.predict(get_dynamics(name, x[0][0],x[0][1], x[0][2], x[0][3], x[0][4], x[0][5], dt[k]))
+            print(s)
+            state[0][i] = s
         x = state
         track.append(state)
+        print(state)
+        k += 1
 
     return track
 
@@ -26,22 +32,23 @@ def get_dynamics(name, x, y, z, vx, vy, vz, dt):
     r = np.sqrt(x * x + y * y + z * z)
 
     if name == 'x':
-        return np.vstack([dt*x, r*x, x/r, vx/r, v*vx, dt*vx/r, dt*v*x, x, vx, r*v*x, v*x/r, v*vx/r, r*x*np.sin(v), vx]).T
+        return np.vstack([dt*x, r*x, x/r, vx/r, v*vx, dt*vx/r, dt*v*x, x, vx, r*v*x, v*x/r, v*vx/r, vx]).T
     elif name == 'y':
-        return np.vstack([dt*y, dt*vy, r*y, y/r, vy/r, v*vy, dt*y/r, dt*vy/r, dt*v*y, dt*y*np.sin(v), y, vy, r*v*y, r*y*np.sin(v), v*y/r, v*vy/r, y*np.sin(v)/r, y, vy]).T
+        return np.vstack([dt*y, dt*vy, r*y, y/r, vy/r, v*vy, dt*y/r, dt*vy/r, dt*v*y, y, vy, r*v*y, v*y/r, v*vy/r, vy]).T
     elif name == 'z':
-        return np.vstack([dt*z, dt*vz, r*z, z/r, vz*z/r, vz/r, v*z, v*vz, dt*r*v, dt*r*vz, dt*z/r, dt*vz/r, dt*v*z, dt*v*vz, z, vz, dt*z*np.sin(v), r*v*z, r*z*np.sin(v)]).T
+        return np.vstack([dt*z, dt*vz, r*z, z/r, vz*z/r, vz/r, v*z, v*vz, dt*r*v, dt*r*vz, dt*z/r, dt*vz/r, dt*v*z, dt*v*vz, z, vz, r*v*z]).T
     elif name == 'vx':
-        return np.vstack([dt*vx, vx/r, v*x, dt*r*x, dt*vx/r, dt*v*x, dt*v*vx, dt*vx*np.sin(v), v*vx/r, vx*np.sin(v)/r,v*x*np.sin(v), x]).T
+        return np.vstack([dt*vx, vx/r, v*x, dt*r*x, dt*vx/r, dt*v*x, dt*v*vx, v*vx/r, x]).T
     elif name == 'vy':
-        return np.vstack([dt*y, dt*vy, vy/r, v*y, dt*vy/r, dt*v*y, dt*v*vy, dt*vy*np.sin(v), v*vy/r, v*y*np.sin(v), v*np.sin(v)/r, y]).T
+        return np.vstack([dt*y, dt*vy, vy/r, v*y, dt*vy/r, dt*v*y, dt*v*vy, v*vy/r, y]).T
     elif name == 'vz':
-        return np.vstack([dt*z, dt*vz, vz/r, v*z, v*vz, dt*r*z, dt*vz/r, dt*v*z, dt*v*vz, dt*vz*np.sin(v), z, v*vz/r, vz*np.sin(v)/r, v*z*np.sin(v)]).T
+        return np.vstack([dt*z, dt*vz, vz/r, v*z, v*vz, dt*r*z, dt*vz/r, dt*v*z, dt*v*vz, z, v*vz/r]).T
 
 
 
 def save_submission(arr):
     np.savetxt('submission.csv', arr, delimiter=',', fmt='%d,%f,%f,%f,%f,%f,%f', header='id,x,y,z,Vx,Vy,Vz')
+
 
 def compute_sindy_for_lorenz():
     clf = linear_model.Lasso(alpha=0.01, max_iter=5000)
@@ -60,33 +67,40 @@ def compute_sindy_for_lorenz():
     print('Score')
     print(clf.score(dmatrix.T, (p_z - c_z) / delta))
 
-
-
-def compute_dynamics(x, y, z, Vx, Vy, Vz, t):
+def compute_dynamics(x, y, z, Vx, Vy, Vz, sx, sy, sz, sVx, sVy, sVz, t):
+#def compute_dynamics(x, y, z, Vx, Vy, Vz, t):
     f = lambda arg_x: (arg_x[1:], arg_x[:-1])
-    (p_t, c_t) = f(t)
-    (p_x, c_x) = f(x)
-    (p_y, c_y) = f(y)
-    (p_z, c_z) = f(z)
 
-    (p_Vx, c_Vx) = f(Vx)
-    (p_Vy, c_Vy) = f(Vy)
-    (p_Vz, c_Vz) = f(Vz)
+    (p_t, c_t) = f(t)
+    dt = np.zeros_like(x)
+    dt[1:] = p_t - c_t
+    (p_x, c_x) = x, sx#f(x)
+    (p_y, c_y) = y, sy#f(y)
+    (p_z, c_z) = z, sz#f(z)
+
+    (p_Vx, c_Vx) = Vx, sVx#f(Vx)
+    (p_Vy, c_Vy) = Vy, sVy#f(Vy)
+    (p_Vz, c_Vz) = Vz, sVz#f(Vz)
 
     arr_p = []
-    for target, name in zip([p_x, p_y, p_z, p_Vx, p_Vy, p_Vz], ['x', 'y', 'z', 'vx', 'vy', 'vz']):
-        dmatrix = get_dynamics(name, c_x, c_y, c_z, c_Vx, c_Vy, c_Vz, p_t - c_t)
+    # clfs = []
+    for target, name in zip([p_x - c_x, p_y - c_y, p_z - c_z, p_Vx - c_Vx, p_Vy - c_Vy, p_Vz - c_Vz], ['x', 'y', 'z', 'vx', 'vy', 'vz']):
+        dmatrix = get_dynamics(name, c_x, c_y, c_z, c_Vx, c_Vy, c_Vz, dt)
+        dmatrix -= dmatrix.mean(axis=0)
+        dmatrix /= dmatrix.std(axis=0)
         #clf = linear_model.Lasso(alpha=1e-3, max_iter=1000)
         clf = linear_model.Ridge()
         #clf = linear_model.HuberRegressor(max_iter=200)
         clf.fit(dmatrix , target)
-        track = get_predicted_track(clf, np.array([c_x[0], c_y[0], c_z[0], c_Vx[0], c_Vy[0], c_Vz[0], (p_t - c_t)[0]]), p_x.shape[0] - 1)
+        #clfs.append(clf)
         arr_p.append(clf.predict(dmatrix))
-            
+        print(f'Coeff {name}\n{clf.coef_}')
+    
+    #track = get_predicted_track(clfs, np.array([c_x[0], c_y[0], c_z[0], c_Vx[0], c_Vy[0], c_Vz[0]]), p_x.shape[0] - 1, p_t-c_t)
     source = np.vstack([c_x, c_y, c_z, c_Vx, c_Vy, c_Vz]).T
     predict = np.vstack(arr_p).T
     target = np.vstack([p_x, p_y, p_z, p_Vx, p_Vy, p_Vz]).T
-    sm = 100 * (1 - idds.smape(target, predict))
+    sm = 100 * (1 - idds.smape(target, predict + source))
     #print(f'Coeff \n{coeff}')
     #sm = 100 * (1 - idds.smape(target, clf.predict(coeff) ))
     print(f'SMAPE {sm}')
@@ -95,48 +109,50 @@ def compute_dynamics(x, y, z, Vx, Vy, Vz, t):
     return sm
 
 
-def compute_sindy(x, y, z, Vx, Vy, Vz, t):
-#def compute_sindy(x, y, z, Vx, Vy, Vz, sx, sy, sz, sVx, sVy, sVz, t):
+#def compute_sindy(x, y, z, Vx, Vy, Vz, t):
+def compute_sindy(x, y, z, Vx, Vy, Vz, sx, sy, sz, sVx, sVy, sVz, t):
     f = lambda arg_x: (arg_x[1:], arg_x[:-1])
     comb = lambda arg_x: [e[0] * e[1] *e[2] for e in list(itertools.combinations(arg_x, 3))]
 
     (p_t, c_t) = f(t)
-    (p_x, c_x) = f(x)
-    (p_y, c_y) = f(y)
-    (p_z, c_z) = f(z)
+    dt = np.zeros_like(x)
+    dt[1:] = p_t - c_t
+    (p_x, c_x) = x, sx#f(x)
+    (p_y, c_y) = y, sy#f(y)
+    (p_z, c_z) = z, sz#f(z)
 
-    (p_Vx, c_Vx) = f(Vx)
-    (p_Vy, c_Vy) = f(Vy)
-    (p_Vz, c_Vz) = f(Vz)
+    (p_Vx, c_Vx) = Vx, sVx#f(Vx)
+    (p_Vy, c_Vy) = Vy, sVy#f(Vy)
+    (p_Vz, c_Vz) = Vz, sVz#f(Vz)
     
     r = np.sqrt(c_x ** 2 + c_y ** 2 + c_z ** 2)
     v = np.sqrt(c_Vx ** 2 + c_Vy ** 2 + c_Vz ** 2)
     
-    c_all = [np.ones_like(r), p_t-c_t, 1/r, r, v, c_x, c_y, c_z, c_Vx, c_Vy, c_Vz, np.sin(r), np.sin(v)]#best
-    dmatrix = np.vstack(comb(c_all) + [p_t-c_t, 1/r, r, v, c_x, c_y, c_z, c_Vx, c_Vy, c_Vz, np.sin(r), np.sin(v)])
+    c_all = [np.ones_like(r), dt, 1/r, r, v, c_x, c_y, c_z, c_Vx, c_Vy, c_Vz, np.sin(r), np.sin(v)]#best
+    dmatrix = np.vstack(comb(c_all) + [dt, 1/r, r, v, c_x, c_y, c_z, c_Vx, c_Vy, c_Vz, np.sin(r), np.sin(v)])
 
     #print(f'The shape of data matrix {dmatrix.T.shape}')
     dmatrix = dmatrix.T# + 0.1 * np.random.randn(*dmatrix.T.shape)
     coeff = []
     intercept = []
-    #dmatrix -= dmatrix.mean(axis=0)
-    #dmatrix /= dmatrix.std(axis=0)
+    dmatrix -= dmatrix.mean(axis=0)
+    dmatrix /= dmatrix.std(axis=0)
 
-    for target in [p_x, p_y, p_z, p_Vx, p_Vy, p_Vz]:
-        #clf = linear_model.Lasso(alpha=1e-3, max_iter=1000)
-        clf = linear_model.Ridge()
+    for target in [p_x - c_x, p_y - c_y, p_z - c_z, p_Vx - c_Vx, p_Vy - c_Vy, p_Vz - c_Vz]:
+        clf = linear_model.Lasso(alpha=1e-2, max_iter=5000)
+        #clf = linear_model.Ridge()
         #clf = linear_model.HuberRegressor(max_iter=10)
         clf.fit(dmatrix , target)
         coeff.append(clf.coef_)
         intercept.append(clf.intercept_)
         #print(clf.coef_)
-        print(clf.intercept_)
+        #print(clf.intercept_)
 
     coeff = np.vstack(coeff).T
     intercept = np.array(intercept).reshape(1, 6)
     source = np.vstack([c_x, c_y, c_z, c_Vx, c_Vy, c_Vz]).T
     target = np.vstack([p_x, p_y, p_z, p_Vx, p_Vy, p_Vz]).T
-    sm = 100 * (1 - idds.smape(target, dmatrix.dot(coeff) + intercept))
+    sm = 100 * (1 - idds.smape(target, dmatrix.dot(coeff) + intercept + source))
     #print(f'Coeff \n{coeff}')
     #sm = 100 * (1 - idds.smape(target, clf.predict(coeff) ))
     print(f'SMAPE {sm}')
